29ed581d0182a67cd2131150dc82beb7dac8cf29
GabrielZanini <gabrielroszan@gmail.com>
2019-05-15 04:38:55 -0300

    Rotação da Camera

---
+++ TCC PUC/Assets/Script/Camera/CameraManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public float landscapeSize = 8f;
+    public float portraitSize = 0f;
+
+    Camera cam;
+    Quaternion landscapeRotation = Quaternion.Euler(0,0,-90);
+
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void Start()
+    {
+        GetPortraitSize();
+    }
+
+    private void Update()
+    {
+        if (Screen.height > Screen.width)
+        {
+            cam.orthographicSize = portraitSize;
+            transform.rotation = landscapeRotation;
+        }
+        else
+        {
+            cam.orthographicSize = landscapeSize;
+            transform.rotation = Quaternion.identity;
+        }
+    }
+
+
+
+    void GetPortraitSize()
+    {
+        float x, y;
+
+        x = Screen.width;
+        y = Screen.height;
+
+        if (x > y)
+        {
+            portraitSize = x/y * landscapeSize;
+        }
+        else
+        {
+            portraitSize = y/x * landscapeSize;
+        }
+    }
+
+
+}
---
+++ TCC PUC/Assets/Script/UI/Debug/DebugCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugCanvas : MonoBehaviour
+{
+    public Text fps;
+
+    string text = "";
+
+    private void Update()
+    {
+        text = "";
+        text += "FPS: " + (1f / Time.deltaTime).ToString("0.0") + "\n";
+        text += "Camera Resolution: " + Camera.main.scaledPixelWidth + "x" + Camera.main.scaledPixelHeight + "\n";
+        text += "Screen Orientation: " + Screen.orientation.ToString() + "\n";
+        text += "Screen Resolution: " + Screen.width + "x" + Screen.height + "\n";
+
+        fps.text = text;
+    }
+}
